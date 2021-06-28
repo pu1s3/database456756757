c3872b49d764088e18b49296da41775f7c88a26e
pu1s3 <73364683+pu1s3@users.noreply.github.com>
2021-06-28 21:33:08 +0100

    asd

--- commands/invite.js
+++ commands/invite.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'invite',
     description: 'sends a invite link',
     execute(message, args, client) {
-        message.channel.send('`https://discord.com/api/oauth2/authorize?client_id=858359508140687360&permissions=8&scope=bot`');
+        message.channel.send('This is my invite: `https://discord.com/api/oauth2/authorize?client_id=858359508140687360&permissions=8&scope=bot`');
     },
 };
